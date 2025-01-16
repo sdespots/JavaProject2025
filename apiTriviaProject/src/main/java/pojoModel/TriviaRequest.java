a39969df5690033fc766c5b73c3fb5163afde9ae
despotst <stefanos.despotis@proton.me>
2025-01-16 20:48:37 +0200

    Maven project build, two POJO classes & jackson dependency

---
+++ apiTriviaProject/src/main/java/pojoModel/TriviaRequest.java
@@ -0,0 +1,49 @@
+package pojoModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+public class TriviaRequest {
+
+	    private String difficulty;
+	    private String category;
+	    private String type;
+	    private String question;
+
+	    @JsonProperty("correct_answer")
+	    private String correctAnswer;
+
+	    @JsonProperty("incorrect_answers")
+	    private List<String> incorrectAnswers;
+
+	    // Getters
+	    public String getDifficulty() { return difficulty; }
+	    public String getCategory() { return category; }
+	    public String getType() { return type; }
+	    public String getQuestion() { return question; }
+	    public String getCorrectAnswer() { return correctAnswer; }
+	    public List<String> getIncorrectAnswers() {return incorrectAnswers; }
+	   
+		// Setters
+	    public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
+	    public void setCategory(String category) { this.category = category; }
+	    public void setType(String type) { this.type = type; }
+	    public void setQuestion(String question) { this.question = question; }
+	    public void setCorrectAnswer(String correctAnswer) { this.correctAnswer = correctAnswer; }
+	    public void setIncorrectAnswers(List<String> incorrectAnswers) { this.incorrectAnswers = incorrectAnswers; }
+	    
+	    @Override
+	    public String toString() {
+	        return "TriviaRequest{" +
+	                "difficulty='" + difficulty + '\'' +
+	                ", category='" + category + '\'' +
+	                ", type='" + type + '\'' +
+	                ", question='" + question + '\'' +
+	                ", correctAnswer='" + correctAnswer + '\'' +
+	                ", incorrectAnswers=" + incorrectAnswers +
+	                '}';
+	    }
+	}
+	
+	
+
---
+++ apiTriviaProject/src/main/java/pojoModel/TriviaResponse.java
@@ -0,0 +1,30 @@
+package pojoModel;
+
+import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TriviaResponse {
+	
+	@JsonProperty("response_code")
+    private int responseCode;
+    private ArrayList<TriviaRequest> results;
+
+    // Getters 
+    
+    public int getResponseCode() {return responseCode;}
+    public ArrayList<TriviaRequest> getResults() {return results;}
+
+    // Setters
+    
+    public void setResponseCode(int responseCode) { this.responseCode = responseCode; }
+    public void setResults(ArrayList<TriviaRequest> results) { this.results = results; }
+    
+    @Override
+    public String toString() {
+        return "TriviaResponse{" +
+               "responseCode=" + responseCode +
+               ", results=" + results +
+               '}';
+    }
+
+}
