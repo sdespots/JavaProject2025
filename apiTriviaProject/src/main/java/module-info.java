7c32b40d6d61d6e598f22b76d0f4bab0bf2df01b
s-koutsoumpa <stella.koutsoumpa12@gmail.com>
2025-01-26 10:02:45 +0200

    Add changes to pom file in apiTriviaProject

--- apiTriviaProject/src/main/java/module-info.java
+++ apiTriviaProject/src/main/java/module-info.java
@@ -1,13 +1,15 @@
 module apiTriviaProject {
-	exports exception;
-	exports pojoModel;
-	exports services;
+    exports exception;
+    exports pojoModel;
+    exports services;
 
-	requires com.fasterxml.jackson.annotation;
-	requires com.fasterxml.jackson.core;
-	requires com.fasterxml.jackson.databind;
-	requires junit;
-	requires org.apache.httpcomponents.httpclient;
-	requires org.apache.httpcomponents.httpcore;
-	requires org.junit.jupiter.api;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.apache.httpcomponents.httpcore;
+
+    
+    requires org.junit.jupiter.api; 
+    requires junit; 
 }
